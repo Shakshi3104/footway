d6648793091b0ebaca05861c810799c394d3cd67
user <watchosastrom3104@gmail.com>
2020-08-11 10:27:12 +0900

    Initial Commit

---
+++ SidewalkSurfaceTypeClassification/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SidewalkSurfaceTypeClassification
+//
+//  Created by Satoshi on 2020/08/11.
+//  Copyright © 2020 Satoshi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
